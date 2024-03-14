5c5d2d5e3df3e3500cd5d5e424c50fd181398d1d
Marcos Fernandes Alves <marcosfernandesalves99@hotmail.com>
2024-03-13 22:29:33 -0300

    feat: removendo teste

--- src/main/java/com/postech30/hackathon/HackathonApplication.java
+++ src/main/java/com/postech30/hackathon/HackathonApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-@EnableSpringDataWebSupport
 @SpringBootApplication
 public class HackathonApplication {
 
