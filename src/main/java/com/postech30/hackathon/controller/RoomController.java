05efd5dcedd05ceafbafd6bf9d5e7d0a3e21e448
Victor Toledo <victorptoledo@outlook.com>
2024-03-15 14:03:34 -0300

    Adição das annotations do swagger nos endpoints

--- src/main/java/com/postech30/hackathon/controller/RoomController.java
+++ src/main/java/com/postech30/hackathon/controller/RoomController.java
@@ -2,7 +2,14 @@ package com.postech30.hackathon.controller;
 
 
 import com.postech30.hackathon.dto.RoomDTO;
+import com.postech30.hackathon.entity.Location;
+import com.postech30.hackathon.entity.Room;
 import com.postech30.hackathon.service.RoomService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,30 +23,80 @@ public class RoomController {
     private RoomService roomService;
 
     @PostMapping
+    @Operation(summary = "Criar um novo quarto.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Room.class))}),
+            @ApiResponse(responseCode = "201", description = "Recurso executado com sucesso.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "401", description = "Não autorizado.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.", content = {@Content(mediaType = "application/json")})
+    })
     public ResponseEntity<RoomDTO> createRoom(@RequestBody RoomDTO roomDTO) {
         RoomDTO createdRoom = roomService.createRoom(roomDTO);
         return ResponseEntity.ok(createdRoom);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar um quarto.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Room.class))}),
+            @ApiResponse(responseCode = "201", description = "Recurso executado com sucesso.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "401", description = "Não autorizado.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.", content = {@Content(mediaType = "application/json")})
+    })
     public ResponseEntity<RoomDTO> updateRoom(@PathVariable String id, @RequestBody RoomDTO roomDTO) {
         RoomDTO updatedRoom = roomService.updateRoom(id, roomDTO);
         return ResponseEntity.ok(updatedRoom);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar um quarto.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Room.class))}),
+            @ApiResponse(responseCode = "201", description = "Recurso executado com sucesso.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "401", description = "Não autorizado.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.", content = {@Content(mediaType = "application/json")})
+    })
     public ResponseEntity<Void> deleteRoom(@PathVariable String id) {
         roomService.deleteRoom(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar quarto por id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Room.class))}),
+            @ApiResponse(responseCode = "201", description = "Recurso executado com sucesso.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "401", description = "Não autorizado.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.", content = {@Content(mediaType = "application/json")})
+    })
     public ResponseEntity<RoomDTO> getRoomById(@PathVariable String id) {
         RoomDTO room = roomService.getRoomById(id);
         return ResponseEntity.ok(room);
     }
 
     @GetMapping
+    @Operation(summary = "Listar todos os quartos.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Room.class))}),
+            @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "401", description = "Não autorizado.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "403", description = "Acesso proibido.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Recurso não encontrado.", content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.", content = {@Content(mediaType = "application/json")})
+    })
     public ResponseEntity<List<RoomDTO>> getAllRooms() {
         List<RoomDTO> rooms = roomService.getAllRooms();
         return ResponseEntity.ok(rooms);
